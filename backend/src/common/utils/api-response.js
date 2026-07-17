c353dbfed1d0fc98d7f11027f7115b93efa31153
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-07-17 17:26:57 +0530

    setup prettier

--- backend/src/common/utils/api-response.js
+++ backend/src/common/utils/api-response.js
@@ -1,11 +1,11 @@
 class ApiResponse {
-    static ok(res, message, data = null) {
-        return res.status(200).json({
-            success: true,
-            message,
-            data
-        });
-    }
+  static ok(res, message, data = null) {
+    return res.status(200).json({
+      success: true,
+      message,
+      data,
+    });
+  }
 }
 
 export default ApiResponse;
--- backend/src/modules/households/households.controller.js
+++ backend/src/modules/households/households.controller.js
@@ -1,45 +1,40 @@
 import ApiResponse from "../../common/utils/api-response.js";
-import * as householdService from './households.service.js'
+import * as householdService from "./households.service.js";
 
 const createHouseHold = async (req, res, next) => {
-    try {
-        const data = await householdService.createHouseHold(req);
-        ApiResponse.ok(res, "Household Created Successfully", data);
-    } catch (err) {
-        next(err);
-    }
-}
-
-const joinHouseHold = async (req, res, next) => {
-    try {
-        const data = await householdService.joinHouseHold(req);
-        ApiResponse.ok(res, "Household Joined Successfully", data);
-    } catch (err) {
-        next (err);
-    }
-}
-
-const getAllMembers = async (req, res, next) => {
-    try{
-        const data = await householdService.getAllMembers(req);
-        ApiResponse.ok(res, "All Members", data);
-    } catch (err) {
-        next(err);
-    }
-}
-
-const leaveHouseHold = async (req, res, next) => {
-    try {
-        const data = await householdService.leaveHouseHold(req);
-        ApiResponse.ok(res, "Left Household Successfully", data);
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const data = await householdService.createHouseHold(req);
+    ApiResponse.ok(res, "Household Created Successfully", data);
+  } catch (err) {
+    next(err);
+  }
 };
 
-export {
-    createHouseHold,
-    joinHouseHold,
-    getAllMembers,
-    leaveHouseHold
-}
+const joinHouseHold = async (req, res, next) => {
+  try {
+    const data = await householdService.joinHouseHold(req);
+    ApiResponse.ok(res, "Household Joined Successfully", data);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getAllMembers = async (req, res, next) => {
+  try {
+    const data = await householdService.getAllMembers(req);
+    ApiResponse.ok(res, "All Members", data);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const leaveHouseHold = async (req, res, next) => {
+  try {
+    const data = await householdService.leaveHouseHold(req);
+    ApiResponse.ok(res, "Left Household Successfully", data);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export { createHouseHold, joinHouseHold, getAllMembers, leaveHouseHold };
